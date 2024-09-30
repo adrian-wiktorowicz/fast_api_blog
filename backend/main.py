ec9ebc37c253ef2b8f3e915ba796b2c902808001
adisongoku <adrian.wiktorowicz440@gmail.com>
2024-09-30 22:03:15 +0200

    connecting to db

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
-from core.config import settings
+from backend.core.config import settings
+
+from backend.db.session import engine
+from backend.db.base_class import Base
 
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_NAME)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_app():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_NAME)
+    create_tables()
+    return app
+
+app = start_app()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def home():
+    return {'msg':'H3H3'}
